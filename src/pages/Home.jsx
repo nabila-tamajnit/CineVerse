3cacf35ec0bf8ba7ac075a34c5fb9830417bd715
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-03-28 04:41:00 +0100

    build Home page structure with all movie and series sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,30 @@ import { Hero } from "../components/Hero";
 import { MoviesSection } from "../components/MoviesSection";
 import { SeriesSection } from "../components/SeriesSection";
 import { EpicSection } from "../components/EpicSection";
+import { LifeStyleSection } from "../components/LifeStyleSection";
+import { useState } from "react";
+import { MovieModal } from "../components/MovieModal";
 
 export const Home = () => {
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
   return (
-    <main className="bg-bg-main min-h-screen text-white">
+    <main>
       <Hero />
-      <MoviesSection />
-      <SeriesSection />
-      <EpicSection />
+      
+      <MoviesSection onCardClick={(movie) => setSelectedMovie(movie)} />
+      <SeriesSection onCardClick={(serie) => setSelectedMovie(serie)} />
+      
+      <EpicSection onCardClick={(item) => setSelectedMovie(item)} />
+      <LifeStyleSection />
+
+      {/* Modale si un film est sélectionné */}
+      {selectedMovie && (
+        <MovieModal 
+          movie={selectedMovie} 
+          onClose={() => setSelectedMovie(null)} 
+        />
+      )}
     </main>
   );
 };
